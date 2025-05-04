a338a26940c4909aaad19a5917a6879f4fee1db5
slinder8 <sjtlinder@icloud.com>
2025-05-04 16:35:03 -0400

    fixed event class in header

--- Event.cpp
+++ Event.cpp
@@ -1,84 +1,48 @@
 #include<string>
 #include "Event.h"
 #include <utility>
-class Event {
-    private:
-    std::string type;
-    public:
-    std::string getType() {
+
+std::string Event::getType() const {
         return type;
+}
+void Event::setType(std::string type) {
+    this->type = std::move(type);
+}
+
+MarketEvent::MarketEvent() {
+    setType("MarketEvent");
+}
+SignalEvent::SignalEvent(std::string symbol, int timeStamp, std::string signalType) {
+    setType("SignalEvent");
+    this->symbol = std::move(symbol);
+    this->timeStamp = timeStamp;
+    this->signalType = std::move(signalType);
+}
+
+OrderEvent::OrderEvent(std::string symbol, std::string orderType, int quantity, bool buy) {
+    setType("OrderEvent");
+    this->symbol = std::move(symbol);
+    this->orderType = std::move(orderType);
+    this->quantity = quantity;
+    this->buy = buy;
+}
+
+
+FillEvent::FillEvent(std::string symbol, int timestamp, int quantity, bool buy, double cost) {
+    setType("FillEvent");
+    this->symbol = std::move(symbol);
+    this->timestamp = timestamp;
+    this->quantity = quantity;
+    this->buy = buy;
+    this->cost = cost;
+    this->commission = getCommission();
+}
+double FillEvent::getCommission() const {
+    double temp = 1.3;
+    if (quantity <= 500) {
+        temp = std::max(temp, .013 * quantity);
+    } else {
+        temp = std::max(temp, .008 * quantity);
     }
-    void setType(std::string type) {
-        this->type = std::move(type);
-    }
-};
-
-class MarketEvent : public Event {
-    public:
-    MarketEvent() {
-        setType("MarketEvent");
-    }
-};
-
-class SignalEvent : public Event {
-    private:
-    std::string symbol;
-    int timeStamp;
-    std::string signalType;
-
-
-    public:
-    SignalEvent(std::string symbol, int timeStamp, std::string signalType) {
-        setType("SignalEvent");
-        this->symbol = std::move(symbol);
-        this->timeStamp = timeStamp;
-        this->signalType = std::move(signalType);
-    }
-};
-
-class OrderEvent : public Event {
-    private:
-    std::string symbol;
-    std::string orderType;
-    int quantity;
-    bool buy; // If true buy, if false sell
-
-
-    public:
-    OrderEvent(std::string symbol, std::string orderType, int quantity, bool buy) {
-        setType("OrderEvent");
-        this->symbol = std::move(symbol);
-        this->orderType = std::move(orderType);
-        this->quantity = quantity;
-        this->buy = buy;
-    }
-};
-
-class FillEvent : public Event {
-    private:
-    std::string symbol;
-    int timestamp;
-    std::string exchange;
-    int quantity;
-    bool buy;
-    double cost;
-    double commission;
-    public:
-    FillEvent(std::string symbol, int timestamp, int quantity, bool buy, double cost) {
-        setType("FillEvent");
-        this->symbol = std::move(symbol);
-        this->timestamp = timestamp;
-        this->quantity = quantity;
-        this->buy = buy;
-        this->cost = cost;
-    }
-    double getCommission() {
-        double temp = 1.3;
-        if (quantity <= 500) {
-            temp = std::max(temp, .013 * quantity);
-        } else {
-            temp = std::max(temp, .008 * quantity);
-        }
-        return temp;
-    }
-};
+    return temp;
+}
--- Event.h
+++ Event.h
@@ -1,10 +1,48 @@
 
 #ifndef EVENT_H
 #define EVENT_H
-//  make as header file
-// class Event {
-//     std::string type;
-//     std::string getType();
-//     void setType(std::string type);
-// };
+class Event {
+    public:
+    std::string type;
+    std::string getType() const;
+    void setType(std::string type);
+};
+
+class MarketEvent : public Event {
+public:
+    MarketEvent();
+};
+
+class SignalEvent : public Event {
+private:
+    std::string symbol;
+    int timeStamp;
+    std::string signalType;
+public:
+    SignalEvent(std::string symbol, int timeStamp, std::string signalType);
+};
+
+class OrderEvent : public Event {
+private:
+    std::string symbol;
+    std::string orderType;
+    int quantity;
+    bool buy; // If true buy, if false sell
+public:
+    OrderEvent(std::string symbol, std::string orderType, int quantity, bool buy);
+};
+
+class FillEvent : public Event {
+private:
+    std::string symbol;
+    int timestamp;
+    std::string exchange;
+    int quantity;
+    bool buy;
+    double cost;
+    double commission;
+public:
+    FillEvent(std::string symbol, int timestamp, int quantity, bool buy, double cost);
+    double getCommission() const;
+};
 #endif //EVENT_H
